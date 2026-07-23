4359bdfefd69b6e0f6988157ba44f38a5bd94f21
MudavathRavi <141152315+MudavathRavi@users.noreply.github.com>
2026-07-23 10:35:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,9 +22,9 @@ module "eks" {
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
-  vpc_id                   = "vpc-07d0f697b6b9f29f7"
-  subnet_ids               = ["subnet-065905130009781aa", "subnet-0c8e4eb3bce95ca0c", "subnet-0d08865e97de748df"]
-  control_plane_subnet_ids = ["subnet-065905130009781aa", "subnet-0c8e4eb3bce95ca0c", "subnet-0d08865e97de748df"]
+  vpc_id                   = "vpc-013eafc041cd4fc12"
+  subnet_ids               = ["subnet-0be0c3c12a4a7d92b", "subnet-086e9ea88dfd8e3ad", "subnet-00995f78544e99c4f"]
+  control_plane_subnet_ids = ["subnet-0be0c3c12a4a7d92b", "subnet-086e9ea88dfd8e3ad", "subnet-00995f78544e99c4f"]
 
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
